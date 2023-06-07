28f41a15b5a6fae0cb80cb8a0e0704ab5f2c4f5e
AlexTimoncini <alextimonci@gmail.com>
2023-06-07 20:32:10 +0200

    added message popup (need to review function to position the popup later)

--- js/script.js
+++ js/script.js
@@ -12,17 +12,20 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            status: 'sent',
+                            triggered: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
-                            status: 'sent'
+                            status: 'sent',
+                            triggered: false,
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
-                            status: 'received'
+                            status: 'received',
+                            triggered: false,
                         }
                     ],
                 },
@@ -34,17 +37,20 @@ createApp({
                         {
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            triggered: false,
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            status: 'received',
+                            triggered: false,
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            triggered: false,
                         }
                     ],
                 },
@@ -56,17 +62,20 @@ createApp({
                         {
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
-                            status: 'received'
+                            status: 'received',
+                            triggered: false,
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            triggered: false,
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
-                            status: 'received'
+                            status: 'received',
+                            triggered: false,
                         }
                     ],
                 },
@@ -78,12 +87,14 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            triggered: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            status: 'received',
+                            triggered: false,
                         }
                     ],
                 },
@@ -95,12 +106,14 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
-                            status: 'sent'
+                            status: 'sent',
+                            triggered: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
-                            status: 'received'
+                            status: 'received',
+                            triggered: false,
                         }
                     ],
                 },
@@ -112,17 +125,20 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
-                            status: 'sent'
+                            status: 'sent',
+                            triggered: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
-                            status: 'received'
+                            status: 'received',
+                            triggered: false,
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
-                            status: 'sent'
+                            status: 'sent',
+                            triggered: false,
                         }
                     ],
                 },
@@ -169,7 +185,7 @@ createApp({
             activeContact: 0,
             newMessage: '',
             lookingFor: false,
-            userSearch: ''
+            userSearch: '',
         }
     },
     methods: {
@@ -182,7 +198,8 @@ createApp({
                 let newMsg = {
                     date: this.doubleDigit(dt.getUTCDate()) + '/' + this.doubleDigit((dt.getMonth() + 1 )) + '/' + dt.getFullYear() + ' ' + this.doubleDigit(dt.getHours()) + ':' + this.doubleDigit(dt.getMinutes()) + ':' + this.doubleDigit(dt.getSeconds()),
                     message: this.newMessage,
-                    status: 'sent'
+                    status: 'sent',
+                    triggered: false,
                 };
                 console.log(this.contactList[this.activeContact].messages)
                 this.contactList[this.activeContact].messages.push(newMsg);
@@ -211,6 +228,15 @@ createApp({
             } else {
                 this.lookingFor = false;
             }
+        },
+        deleteMsgPopUp(messageObject){
+            messageObject.triggered = !messageObject.triggered
+        },
+        msgPosition(msgIndex){
+            if(msgIndex < 10){
+                return true
+            }
+            return false
         }
     }
 }).mount('#app')
